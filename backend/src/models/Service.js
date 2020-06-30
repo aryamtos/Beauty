324bdbbe79a97227cedd6ba7b5bce2a2c4ea8c5a
LeoDNascimento <leonardo.nascimento1306@gmail.com>
2020-06-30 20:16:08 -0300

    Colocando o model service de leo

--- backend/src/models/Service.js
+++ backend/src/models/Service.js
@@ -1,48 +1,26 @@
 'use strict'
 
-const mongoose = require('mongoose'); //importamos o mongoose
+const mongoose = require('mongoose');
 
-const serviceModel = new mongoose.Schema({ //definição de campos
-    //localidade - endereco, contato, horários de funcionamento - profissionais
-    foto:{
-        type: String,
-         required:false,
-    },
-    nome: { type: String, required: false},//nome do Local
-    categoria:{type:[String]},
-    descricao: {type: String, required: false},
-    servicos: {
+const serviceModel = new mongoose.Schema({
+    nomeService:{ type: String, required: true, trim: true, index: true },
+    nome: { type: String, required: true, trim: true, index: true },
+    parte: { type: String, required: true, trim: true, index: true },
+    preco: { type: Number, required: true, default: 0 },
+    tempo: { type: Number, required: true, default: 0 },
+    dataCriacao: { type: Date, default: Date.now },
+    user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'Servicos',
-    },
-    address: { type: String, required: false, trim: true },
-    user:{
-        type: mongoose.Schema.Types.ObjectId,      
-        ref:'User'
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now(),
-
-    },
-},{
-    toJSON:{
-        virtuals:true,
-    },
-});
+        require: true,
+        ref: 'UserPartner'
+    }
+}, { versionKey: false });
 
 serviceModel.pre('save', next => {
-    let now = new Date();
-    if (!this.createdAt)
-        this.createdAt = now;
+    let agora = new Date();
+    if (!this.dataCriacao)
+        this.dataCriacao = agora;
     next();
 });
 
-serviceModel.virtual('foto_url').get(function(){
-    return `http://192.168.1.109:3000/files/${this.foto}`;
-});
-
-
 module.exports = mongoose.model('Service', serviceModel);
-
-
