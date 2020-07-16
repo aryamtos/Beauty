67d305566839e0f67c69d9447f9af535a972c51e
brenu <bvsousa64@gmail.com>
2020-07-15 23:29:20 -0300

    :rocket: Implementa agendamento de serviços

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,69 +1,60 @@
-const Booking = require('../models/Booking');
-const { isPast } = require('date-fns');
+const Booking = require("../models/Booking");
+const { isPast } = require("date-fns");
 
 const {
-    startOfDay,
-    endOfDay,
-    startOfWeek,
-    endOfWeek,
-    startOfMonth,
-    endOfMonth,
-    startOfYear,
-    endOfYear
-} = require('date-fns');
+  startOfDay,
+  endOfDay,
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth,
+  startOfYear,
+  endOfYear,
+} = require("date-fns");
 
 const current = new Date();
 module.exports = {
+  async index(req, res) {
+    const { service_id } = req.query;
+    const servicos = await Booking.find({ service: service_id }).sort("date"); //encontrar vários tipos
 
-    async index(req, res) {
+    return res.json(servicos);
+  },
+  async store(req, res, next) {
+    const { user_id } = req.headers;
+    const service_id = req.params.id; //spot_id
+    const { date } = req.body;
 
-        const { service_id } = req.query;
-        const servicos = await Booking.find({ service: service_id }).sort('date'); //encontrar vários tipos
+    const booking = await Booking.create({
+      user: user_id,
+      service: service_id,
+      date,
+    });
+    await booking.populate("service").populate("user").execPopulate();
 
-        return res.json(servicos);
-    },
-    async store(req, res, next) {
+    const ownerSocket = req.connectedUsers[booking.service.user_id];
 
-        const { user_id } = req.headers;
-        const { service_id } = req.params;//spot_id
-        const { date } = req.body;
+    if (ownerSocket) {
+      req.io.to(ownerSocket).emit("booking_request", booking);
+    }
 
-        const booking = await Booking.create({
+    return res.json(booking);
+  },
 
-            user: user_id,
-            service: service_id,
-            date,
-
-        });
-        await booking.populate('service').populate('user').execPopulate();
-
-        const ownerSocket = req.connectedUsers[booking.service.user_id];
-
-        if (ownerSocket) {
-            req.io.to(ownerSocket).emit('booking_request', booking)
-        }
-        
-        return res.json(booking);
-
-    },
-    
-    async getAll(req, res){
-        const { categoria } = req.query;
-        const servicos = await Booking.find();
-        return res.json(servicos);
-    },
-    async show(req, res) {
-        await Booking.findById(req.params.id)
-            .then(response => {
-                if (response)
-                    return res.status(200).json(response);
-                else
-                    return res.status(404).json({ error: 'Agendamento não encontrado' });
-            })
-            .catch(error => {
-                return res.status(500).json(error);
-            })
-    },
-    
-
+  async getAll(req, res) {
+    const { categoria } = req.query;
+    const servicos = await Booking.find();
+    return res.json(servicos);
+  },
+  async show(req, res) {
+    await Booking.findById(req.params.id)
+      .then((response) => {
+        if (response) return res.status(200).json(response);
+        else
+          return res.status(404).json({ error: "Agendamento não encontrado" });
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  },
 };
--- mobile/src/pages/BookRequest.js
+++ mobile/src/pages/BookRequest.js
@@ -1,120 +1,146 @@
-import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet, ScrollView, View, StatusBar, FlatList, Image, SafeAreaView, Alert, TextInput, AsyncStorage } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Icon } from 'react-native-elements';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  StyleSheet,
+  ScrollView,
+  View,
+  StatusBar,
+  FlatList,
+  Image,
+  SafeAreaView,
+  Alert,
+  TextInput,
+  AsyncStorage,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Icon } from "react-native-elements";
 
-import GlobalStyles from '../assets/GlobalStyles';
-import lupa from '../assets/BUSCAR_cinza.png';
-import logoLoja from '../assets/logo_loja.jpg';
-import PropTypes from 'prop-types';
-import { useNavigation, useRoute } from '@react-navigation/native'
+import GlobalStyles from "../assets/GlobalStyles";
+import lupa from "../assets/BUSCAR_cinza.png";
+import logoLoja from "../assets/logo_loja.jpg";
+import PropTypes from "prop-types";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-
-import api from '../services/api';
+import api from "../services/api";
 
 //import ServiceList from '../components/ServiceList';x'
 
 //import { Container, Title, Button, ButtonText, ProductList } from './styles';
 
 export default function BookRequest({ navigation }) {
+  const route = useRoute();
+  const [date, setDate] = useState("");
+  const service_id = route.params._id;
+  //const user_id = servico.user;
+  //console.log(user_id)
+  async function handleSubmit() {
+    /**
+     * -----------------------------------------
+     *    Pegando user do AsyncStorage
+     * -----------------------------------------
+     *
+     * Como você usou stringify na hora de arma-
+     * zenar o user, essa string precisa ser
+     * formatada para um JSON novamente, então
+     * se usa o JSON.parse() :)
+     */
+    let user = await AsyncStorage.getItem("user");
+    user = JSON.parse(user);
+    const user_id = user._id;
 
-    const route = useRoute();
-    const [date, setDate] = useState('');
-    const servico = route.params._id;
-    console.log(servico)
-    //const user_id = servico.user;
-    //console.log(user_id)
-    async function handleSubmit(){
-       // const user_id = await AsyncStorage.getItem('user');
-       const user_id = await AsyncStorage.getItem('_id')
-        await api.post(`/service/${servico}/bookings`,{
-            date
-        },{
-          headers:{user_id}
-        })
-   
-    Alert.alert('Solicitação de serviço enviada;');
+    console.log(service_id);
 
-    navigation.navigate('StoreProfile');
-
-}
-    function handleCancel(){
-
-        navigation.navigate('StoreProfile');
+    try {
+      await api.post(
+        `/service/${service_id}/bookings`,
+        {
+          date,
+        },
+        {
+          headers: { "Content-Type": "application/json", user_id: user_id },
+        }
+      );
+    } catch (error) {
+      console.log(error);
     }
-    
-    return(
-        < SafeAreaView style = {styles.container}>
-        <Text style={styles.label}>DATA DE INTERESSE *</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Qual data você quer reservar?"
-            placeholderTextColor='#999'
-            autoCapitalize="none"
-            autoCorrect={false}
-            value={date}
-            onChangeText={setDate}
-        />
-        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-        <Text style={styles.buttonText}>Solicitar Serviço</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={handleCancel} style={styles.cancelButton}>
+    Alert.alert("Solicitação de serviço enviada;");
+
+    navigation.navigate("StoreProfile");
+  }
+  function handleCancel() {
+    navigation.navigate("StoreProfile");
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Qual data você quer reservar?"
+        placeholderTextColor="#999"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar Serviço</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCancel} style={styles.cancelButton}>
         <Text style={styles.buttonText}>Cancelar </Text>
-        </TouchableOpacity>
-    </SafeAreaView >
-    );
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
 /*
  <SafeAreaView style={[GlobalStyles.droidSafeArea, styles.container]} >*/
 
 const styles = StyleSheet.create({
-
   container: {
-  
     margin: 30,
     marginTop: 50,
     marginTop: StatusBar.currentHeight,
- 
   },
   label: {
-    fontWeight: 'bold',
-    color: '#444',
+    fontWeight: "bold",
+    color: "#444",
     marginBottom: 8,
-    marginTop: 30
-},
+    marginTop: 30,
+  },
 
-input: {
+  input: {
     borderWidth: 1,
-    borderColor: '#DDD',
+    borderColor: "#DDD",
     paddingHorizontal: 20,
     fontSize: 16,
-    color: '#444',
+    color: "#444",
     height: 44,
     marginBottom: 20,
-    borderRadius: 2
-},
-
-button: {
-    height: 42,
-    backgroundColor: '#483D8B',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 2
-},
-
-cancelButton: {
-    height: 42,
-    backgroundColor: '#ccc',
-    justifyContent: 'center',
-    alignItems: 'center',
     borderRadius: 2,
-    marginTop: 10
-},
+  },
 
-buttonText: {
-    color: '#FFF',
-    fontWeight: 'bold',
-    fontSize: 16
-}
+  button: {
+    height: 42,
+    backgroundColor: "#483D8B",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+  },
+
+  cancelButton: {
+    height: 42,
+    backgroundColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
 });
