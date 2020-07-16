64d399a3bd90faf86e8ce6017fc5cf8e64f5ae12
brenu <bvsousa64@gmail.com>
2020-07-16 12:34:12 -0300

    :bug: Liga serviços criados aos seus respectivos criadores

--- backend/src/controllers/ServiceControllers.js
+++ backend/src/controllers/ServiceControllers.js
@@ -1,63 +1,104 @@
-'use strict'
+"use strict";
 
-const repository = require('./repository/serviceRepository');
-const ctrlBase = require('../config/base/controller-base');
-const validation = require('../config/helpers/validation');
+const repository = require("./repository/serviceRepository");
+const ctrlBase = require("../config/base/controller-base");
+const validation = require("../config/helpers/validation");
 const _repo = new repository();
-const Service = require('../models/Service');
+const Service = require("../models/Service");
+const UserPartner = require("../models/UserPartner");
 
-function serviceController() {
+function serviceController() {}
 
-}
-
+/**
+ * ------------------------------------------
+ *      Observação
+ * ------------------------------------------
+ *
+ *  Esta função estava criando os serviços,
+ *  mas não estava os ligando com o model
+ *  de usuário no banco de dados.
+ *
+ *  A listagem só estava funcionando porque
+ *  tal busca faz referência direta à tabela
+ *  de serviços, em vez de usar a tabela
+ *  users como mediadora.
+ *
+ *  Agora, ela liga novos serviços aos seus
+ *  respectivos criadores :)
+ */
 serviceController.prototype.post = async (req, res) => {
-    let _validationContract = new validation();
-    _validationContract.isRequired(req.body.nome, 'O nome do produto é obrigatorio');
-    _validationContract.isRequired(req.body.descricao, 'A descrição do produto é obrigatoria');
-    _validationContract.isRequired(req.body.preco, 'O preço do produto é obrigatorio');
+  let _validationContract = new validation();
+  _validationContract.isRequired(
+    req.body.nome,
+    "O nome do produto é obrigatorio"
+  );
+  _validationContract.isRequired(
+    req.body.descricao,
+    "A descrição do produto é obrigatoria"
+  );
+  _validationContract.isRequired(
+    req.body.preco,
+    "O preço do produto é obrigatorio"
+  );
 
-    if (req.body.preco)
-        _validationContract.isTrue(req.body.preco == 0, 'O preço do produto deve ser maior que Zero.');
+  if (req.body.preco)
+    _validationContract.isTrue(
+      req.body.preco == 0,
+      "O preço do produto deve ser maior que Zero."
+    );
 
-    // ctrlBase.post(_repo, _validationContract, req, res);
-    const {nomeService, nome, parte,tempo,  preco } = req.body;
-    const {user_id} = req.headers;
+  // ctrlBase.post(_repo, _validationContract, req, res);
+  const { nomeService, nome, parte, tempo, preco } = req.body;
+  const { user_id } = req.headers;
 
-    const service = await Service.create({
-        nomeService,
-        user: user_id,
-        nome,
-        parte,
-        tempo,
-        preco
-    })
-    await service.populate('user').execPopulate();
-    return res.json(service);
+  const user = await UserPartner.findById(user_id);
+
+  const service = await Service.create({
+    nomeService,
+    user: user_id,
+    nome,
+    parte,
+    tempo,
+    preco,
+  });
+
+  user.servicos.push(service);
+  await user.save();
+
+  return res.json(service);
 };
 
 serviceController.prototype.put = async (req, res) => {
-    let _validationContract = new validation();
+  let _validationContract = new validation();
 
-    
-    _validationContract.isRequired(req.body.tempo, 'O tempo do serviço é obrigatoria');
-    _validationContract.isRequired(req.body.preco, 'O preço do serviço é obrigatorio');
+  _validationContract.isRequired(
+    req.body.tempo,
+    "O tempo do serviço é obrigatoria"
+  );
+  _validationContract.isRequired(
+    req.body.preco,
+    "O preço do serviço é obrigatorio"
+  );
 
-    if (req.body.preco)
-        _validationContract.isTrue(req.body.preco <= 0, 'O preço do produto deve ser maior que Zero.');
+  if (req.body.preco)
+    _validationContract.isTrue(
+      req.body.preco <= 0,
+      "O preço do produto deve ser maior que Zero."
+    );
 
-    ctrlBase.put(_repo, _validationContract, req, res);
+  ctrlBase.put(_repo, _validationContract, req, res);
 };
 
 serviceController.prototype.get = async (req, res) => {
-    ctrlBase.get(_repo, req, res);
+  ctrlBase.get(_repo, req, res);
 };
 
 serviceController.prototype.getById = async (req, res) => {
-    ctrlBase.getById(_repo, req, res);
+  ctrlBase.getById(_repo, req, res);
 };
 
 serviceController.prototype.delete = async (req, res) => {
-    ctrlBase.delete(_repo, req, res);
+  ctrlBase.delete(_repo, req, res);
 };
 
 module.exports = serviceController;
--- backend/src/models/UserPartner.js
+++ backend/src/models/UserPartner.js
@@ -1,33 +1,36 @@
 //model que irá teer o schema dos usuarios da aplicação (parceiros)
 //importar o mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //criando o schema ("tabela") dos parceiros do app
-const UserPartnerSchema = new mongoose.Schema({
-
+const UserPartnerSchema = new mongoose.Schema(
+  {
     logo: { type: String },
     responsibleName: { type: String, required: true, trim: true, index: true },
-    category: {trim: true, type:String},
+    category: { trim: true, type: String },
     interpriseName: { type: String, trim: true, index: true },
-    cpf: {type: String, required: true},
-    phone: {type: Number, required: true},
+    cpf: { type: String, required: true },
+    phone: { type: Number, required: true },
     adress: { type: String, required: true },
     about: String,
     email: { type: String, required: true },
     senha: { type: String, required: true },
     dataCriacao: { type: Date, default: Date.now },
-    isAdmin: {type: Boolean, default: false},
-    servicos:{
+    isAdmin: { type: Boolean, default: false },
+    servicos: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Service'
-    }
-}, { versionKey: false });
+        ref: "Service",
+      },
+    ],
+  },
+  { versionKey: false }
+);
 
-UserPartnerSchema.pre('save', next => {
-    let agora = new Date();
-    if (!this.criationDate)
-        this.criationDate = agora;
-    next();
+UserPartnerSchema.pre("save", (next) => {
+  let agora = new Date();
+  if (!this.criationDate) this.criationDate = agora;
+  next();
 });
 
-module.exports = mongoose.model('UserPartner', UserPartnerSchema);
+module.exports = mongoose.model("UserPartner", UserPartnerSchema);
