0fd63a36b36dd955d9154fbc76be5f06bceeb21c
brenu <bvsousa64@gmail.com>
2020-07-26 09:54:02 -0300

    :bug: Fix partner automatically deleting services on web service

--- frontend/src/pages/Delete/index.js
+++ frontend/src/pages/Delete/index.js
@@ -1,30 +1,28 @@
-import React, {useState} from 'react';
-import api from '../../services/api';
+import React, { useState } from "react";
+import api from "../../services/api";
 
-import './styles.css';
+import "./styles.css";
 
-export default function Delete({history}){
+export default function Delete({ history }) {
+  async function handleDelete() {
+    const user_id = localStorage.getItem("partner");
+    const service_id = localStorage.getItem("productID");
 
-    async function handleDelete(){
-        const user_id = localStorage.getItem('partner');
-        const service_id = localStorage.getItem('productID');
+    const token_access = localStorage.getItem("token-access");
+    const response = await api.delete("partner/service/delete/" + service_id, {
+      headers: { user_id, token_access },
+    });
+    history.push("/dashboard");
+  }
 
-        const token_access = localStorage.getItem('token-access');
-        const response = await api.delete('partner/service/delete/' + service_id,
-             {  
-                headers: {user_id, token_access}
-            },     
-         )
-      history.push('/dashboard');
-    }
-
-    return (
-        <>    
-           <p>Deseja realmente deletar esse serviço?</p>
-           <span className="sbutton"> 
-            <button onClick={handleDelete()} className="btn2">Deletar</button>
-            </span>          
-        </>
-
-    )
+  return (
+    <>
+      <p>Deseja realmente deletar esse serviço?</p>
+      <span className="sbutton">
+        <button onClick={handleDelete} className="btn2">
+          Deletar
+        </button>
+      </span>
+    </>
+  );
 }
