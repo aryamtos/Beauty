3beb8e71be7b341ee5053b64af70f9e0a1036876
brenu <bvsousa64@gmail.com>
2020-07-26 23:45:07 -0300

    :art: Create interface of the businessHours edit page

--- frontend/src/pages/BusinessHoursUpdate/index.js
+++ frontend/src/pages/BusinessHoursUpdate/index.js
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import "./styles.css";
+
+import api from "../../services/api";
 
 export default function BusinessHoursUpdate() {
+  const [businessHours, setBusinessHours] = useState([]);
+
+  useEffect(() => {
+    async function handleInit() {
+      const partner_id = localStorage.getItem("partner");
+      try {
+        const response = await api.get("/businesshour", {
+          headers: {
+            partner_id,
+          },
+        });
+
+        if (response.data) {
+          setBusinessHours(response.data.businessHours);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    handleInit();
+  }, []);
+
+  useEffect(() => {
+    if (!businessHours[0]) {
+      businessHours.push({ dia: "Dom", horaInicio: "", horaFim: "" });
+      businessHours.push({ dia: "Seg", horaInicio: "", horaFim: "" });
+      businessHours.push({ dia: "Ter", horaInicio: "", horaFim: "" });
+      businessHours.push({ dia: "Qua", horaInicio: "", horaFim: "" });
+      businessHours.push({ dia: "Qui", horaInicio: "", horaFim: "" });
+      businessHours.push({ dia: "Sex", horaInicio: "", horaFim: "" });
+      businessHours.push({ dia: "Sab", horaInicio: "", horaFim: "" });
+    }
+  }, [businessHours]);
+
+  async function handleSubmit() {
+    //
+  }
+
   return (
-    <div className="teste">
-      <h1>Oi</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2 style={{ marginBottom: 20 }}>Seus horários</h2>
+      {businessHours[0] && (
+        <>
+          {businessHours.map((item) => (
+            <div className="dayContainer">
+              <label>
+                <p className="dayLabel">{item.dia}</p>
+              </label>
+              <input
+                className="hourInput"
+                placeholder="00:00"
+                value={item.horaInicio}
+              />
+              <input
+                className="hourInput"
+                placeholder="00:00"
+                value={item.horaFim}
+              />
+            </div>
+          ))}
+        </>
+      )}
+      <button type="submit" className="btn">
+        Atualizar
+      </button>
+    </form>
   );
 }
