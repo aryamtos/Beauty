6e9fd5b5bb9e7e6f16030c9fa5288c1f3616fc28
brenu <bvsousa64@gmail.com>
2020-07-27 11:12:54 -0300

    :tada: Create controller for business hours massive changes

---
+++ backend/src/controllers/BusinessHourMassiveController.js
@@ -0,0 +1,14 @@
+const BusinessHour = require("../models/BusinessHour");
+const UserPartner = require("../models/UserPartner");
+
+const BusinessHourController = require("../controllers/BusinessHourController");
+
+module.exports = {
+  async store(req, res) {
+    //
+  },
+
+  async update(req, res) {
+    //
+  },
+};
