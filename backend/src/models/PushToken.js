7f20f4053e8a425f7ce57c2ee0fe2b86e2d8c080
therealbrenu <bvsousa64@gmail.com>
2020-08-25 07:51:13 -0300

    :rocket: Create PushToken schema

---
+++ backend/src/models/PushToken.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const PushToken = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    partner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserPartner",
+    },
+    token: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("PasswordReset", PushToken);
