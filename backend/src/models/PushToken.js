c98b0eb131aee5a9dbca271ca6289e72c8c8f1b2
therealbrenu <bvsousa64@gmail.com>
2020-09-03 16:27:44 -0300

    :rocket: Set date in order to expire the push tokens

--- backend/src/models/PushToken.js
+++ backend/src/models/PushToken.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
-const PushToken = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    partner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "UserPartner",
-    },
-    token: String,
+const PushToken = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
-  { timestamps: true }
-);
+  partner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "UserPartner",
+  },
+  token: String,
+  createdAt: {
+    type: Date,
+    expires: "10d",
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("PushToken", PushToken);
